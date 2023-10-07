e964abca16f09c676ebff42e026c953eabdd6f5a
itssinghankit <singhankit.kr@gmail.com>
2023-10-07 15:14:13 +0530

    added top app bar

--- app/src/main/java/com/example/tweetsy/MainActivity.kt
+++ app/src/main/java/com/example/tweetsy/MainActivity.kt
@@ -1,35 +1,35 @@
 package com.example.tweetsy
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.tweetsy.api.TweetsyInterfaceApi
 import com.example.tweetsy.screens.CategoryScreen
 import com.example.tweetsy.screens.TweetScreen
 import com.example.tweetsy.ui.theme.TweetsyTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-//    @Inject
+    //    @Inject
 //    lateinit var tweetsyInstanceAPI : TweetsyInterfaceApi
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,7 +40,20 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TweetsyTheme {
-                App()
+                Scaffold(modifier = Modifier.fillMaxWidth(), topBar = {
+                    TopAppBar(
+                        title = { Text(text = "Tweetsy") },
+                        colors = TopAppBarDefaults.smallTopAppBarColors(
+                            containerColor = Color.Black,
+                            titleContentColor = Color.White
+                        )
+                    )
+                }) {
+                    Box(modifier = Modifier.padding(it)) {
+                        App()
+                    }
+                }
+//                App()
 //                TweetScreen()
 //               CategoryScreen()
             }
@@ -51,19 +64,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
-    val navController= rememberNavController()
-    NavHost(navController = navController, startDestination = "category"){
-        composable(route = "category"){
-            CategoryScreen{
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "category") {
+        composable(route = "category") {
+            CategoryScreen {
                 navController.navigate("detail/${it}")
             }
         }
         //passing the category to check the category of tweets we want
-        composable(route = "detail/{category}", arguments= listOf(
-            navArgument("category"){
-                type= NavType.StringType
+        composable(route = "detail/{category}", arguments = listOf(
+            navArgument("category") {
+                type = NavType.StringType
             }
-        )){
+        )) {
             TweetScreen()
         }
     }
