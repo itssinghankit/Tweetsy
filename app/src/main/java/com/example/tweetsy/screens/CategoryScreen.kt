ba73e4a03aecf9470cfba837c4746660661ced5b
itssinghankit <singhankit.kr@gmail.com>
2023-10-09 13:34:34 +0530

    added loading screen and app inspector

--- app/src/main/java/com/example/tweetsy/screens/CategoryScreen.kt
+++ app/src/main/java/com/example/tweetsy/screens/CategoryScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -37,13 +38,19 @@ fun CategoryScreen(onclick:(category:String)->Unit) {
     val categoryViewModel: CategoryViewModel = hiltViewModel()
     val categories: State<List<String>> = categoryViewModel.category.collectAsState()
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.SpaceAround
-    ) {
-        items(categories.value) {
-            CatagoryItem(category = it,onclick)
+    if(categories.value.isEmpty()){
+        Box(modifier = Modifier.fillMaxSize(1f), contentAlignment = Alignment.Center){
+            Text(text = "Loading...", fontSize = 18.sp)
+        }
+    }else{
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            items(categories.value) {
+                CatagoryItem(category = it,onclick)
+            }
         }
     }
 }
